0d43767bfae1258c5b1a48726ea0b3066eb31728
Patrick Cuba <cubap@slu.edu>
2020-05-21 10:42:31 -0500

    project subfolders
    newberry-french
    newberry-italian

--- src/java/edu/slu/tpen/servlet/LoginServlet.java
+++ src/java/edu/slu/tpen/servlet/LoginServlet.java
@@ -74,9 +74,6 @@ public class LoginServlet extends HttpServlet {
             if (u.getUID() > 0) {
                HttpSession sess = req.getSession(true);
                sess.setAttribute("UID", u.getUID());
-//               System.out.println("HAve UID!!!!!!!!!!");
-//               System.out.println(u.getUID());
-//               System.out.println(sess.getAttribute("UID"));
                PrintWriter writer = resp.getWriter();
                writer.print(sess.getId());
             } else {
@@ -84,7 +81,6 @@ public class LoginServlet extends HttpServlet {
             }
          } else if (mail == null && password == null) {
             // Passing null data indicates a logout.
-            // System.out.println("Email and pwd null   !!!!!!!!!!");
             HttpSession sess = req.getSession(true);
             sess.removeAttribute("UID");
             resp.setStatus(SC_NO_CONTENT);
